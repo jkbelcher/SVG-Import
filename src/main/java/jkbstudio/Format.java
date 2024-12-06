0e0b609de2af1e3128db6130d420a9c811cfd283
Justin Belcher <jkbelcher@gmail.com>
2024-12-06 07:46:54 -0700

    Create static formatter

--- src/main/java/jkbstudio/Format.java
+++ src/main/java/jkbstudio/Format.java
@@ -10,13 +10,8 @@ import heronarts.lx.parameter.LXParameter;
 
 import java.text.DecimalFormat;
 
-public enum Format implements LXParameter.Formatter {
-  DECIMAL_CLEAN {
-    private static final DecimalFormat decimalFormat = new DecimalFormat("#0.##");
+public class Format {
+  private static final DecimalFormat decimalFormat = new DecimalFormat("#0.##");
 
-    @Override
-    public String format(double value) {
-      return decimalFormat.format(value);
-    }
-  };
+  public static final LXParameter.Formatter DECIMAL_CLEAN = decimalFormat::format;
 }
